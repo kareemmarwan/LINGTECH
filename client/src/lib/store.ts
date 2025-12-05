69b45e331dcbd586844064f9fe1d1bcde9d69033
Kareem Marwan <kareemkemoo23@gmail.com>
2025-12-05 14:31:31 +0200

    update

--- client/src/lib/store.ts
+++ client/src/lib/store.ts
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
+// import authReducer from './slices/authSlice';
 import coursesReducer from './slices/coursesSlice';
 import teamReducer from './slices/teamSlice';
 import storiesReducer from './slices/storiesSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    // auth: authReducer,
     courses: coursesReducer,
     team: teamReducer,
     stories: storiesReducer,
