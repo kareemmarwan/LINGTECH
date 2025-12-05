00013af222289aa8d9e8f097b3f894c7c021b4a5
Kareem Marwan <kareemkemoo23@gmail.com>
2025-12-05 14:09:58 +0200

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
+// import { queryClient } from "./lib/queryClient";
+// import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Provider } from "react-redux";
