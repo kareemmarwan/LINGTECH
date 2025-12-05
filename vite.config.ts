d424fdff1b5dfcdd8baa131c94ffe64da04040df
Kareem Marwan <kareemkemoo23@gmail.com>
2025-12-05 14:54:06 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,8 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    // outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: "dist",
     emptyOutDir: true,
     chunkSizeWarningLimit: 1500
   },
